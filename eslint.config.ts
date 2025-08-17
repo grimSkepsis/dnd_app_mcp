bb5d2ca584f49f488a94c865318cba6f9e775f83
grimSkepsis <developer.mikehofer@gmail.com>
2025-08-17 12:51:27 -0700

    grim/fixes eslint setup

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,15 +1,20 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
+export default [
+  {
+    ignores: [
+      "build/**/*",
+      "src/generated/**/*",
+      "node_modules/**/*",
+      "dist/**/*",
+    ],
+  },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
-    extends: ["js/recommended"],
     languageOptions: { globals: globals.node },
-    ignores: ["build/"],
   },
-  tseslint.configs.recommended,
-]);
+  ...tseslint.configs.recommended,
+];
